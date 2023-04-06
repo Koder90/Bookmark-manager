abe0d7a0483abd66222648babdb957b1b4bb62cc
Koder90 <jerko.zlopasa@hotmail.com>
2023-04-06 13:31:21 +0200

    3 files modified, 1 added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
+import bookmarkLogo from '../utils/logo-bookmark.svg';
+
 export default function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <Link href="/">Features</Link>
-      <Link href="/">Pricing</Link>
-      <Link href="/">Contact</Link>
+    <div className={styles.container}>
+      <div className={styles.bookmark}>
+        <img src={bookmarkLogo} />
+      </div>
+      <div className={styles.navbar}>
+        <Link href="/">FEATURES</Link>
+        <Link href="/">PRICING</Link>
+        <Link href="/">CONTACT</Link>
+        <div className={styles.btn}>LOGIN</div>
+      </div>
     </div>
   );
 }
